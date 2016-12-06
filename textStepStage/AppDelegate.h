c7300a1cdf657ca71444abd66f69f4d34cf0d38e
Gust50 <18647703450@163.com>
2016-12-06 17:25:26 +0800

    测试用的1

---
+++ textStepStage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textStepStage
+//
+//  Created by kobe on 16/12/6.
+//  Copyright © 2016年 Gust50. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textStepStage/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  textStepStage
+//
+//  Created by kobe on 16/12/6.
+//  Copyright © 2016年 Gust50. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+@end
